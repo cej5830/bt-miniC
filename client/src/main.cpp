9ceefc56bd65cdd8ae0eeaaa637e2cf78804c1a3
Charles Johnson <cej1922@jagmail.southalabama.edu>
2025-10-28 15:40:29 -0500

    File reading

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -12,7 +12,7 @@ namespace http = boost::beast::http;
 namespace urls = boost::urls;
 
 int main(int argc, char **argv) {
-  // Args: host port infohash peer_id my_port [event]
+  // Args: host port infohash peer_id my_port [event] torrent_file
   if (argc < 6) {
     std::cerr << "Usage: " << argv[0]
               << " <host> <port> <infohash> <peer_id> <my_port> [event]\n"
@@ -21,12 +21,43 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+
   const std::string host = argv[1];
   const std::string svc_port = argv[2];
   const std::string infohash = argv[3];
   const std::string peer_id = argv[4];
   const std::string my_port = argv[5];
   const std::string event = (argc > 6 ? argv[6] : "");
+  const std::string torrentFileName = argv[7];
+
+  //Everything for getting information from the torrent file
+  auto testY = std::vector<unsigned char>(std::istreambuf_iterator<char>(t), std::istreambuf_iterator<char>());
+  auto [start1, end1] = extract_info_range(testY);
+  std::span<const unsigned char> info_bytes(testY.data() + start1, end1 - start1);
+  unsigned char hash1[SHA_DIGEST_LENGTH];
+  SHA1(info_bytes.data(), info_bytes.size(), hash1); //Getting infohash
+  std::string fileInfoHash = toHex(hash1, SHA_DIGEST_LENGTH)
+
+  t.seekg(0, std::ios::end);
+  std::size_t size = t.tellg();
+  std::string buffer(size, '\0');
+  t.seekg(0);
+  t.read(&buffer[0], size); //Reading from the file for information
+
+  auto decodedFile = bencode::decode(buffer);
+  auto fileAnnounce = decodedFile["announce"];
+  auto fileComment = decodedFile["comment"];
+  auto infoDict = decodedFile["info"];
+  auto fL2 = decodedFile["length2"];
+  auto fileLength = infoDict["length"];
+  auto fileName = infoDict["name"];
+  auto filePieceLength = infoDict["piece length"];
+  auto anno = std::get<bencode::string>(fileAnnounce);
+  auto comm = std::get<bencode::string>(fileComment);
+  auto fN = std::get<bencode::string>(fileName);
+  auto len = std::get<bencode::integer>(fileLength);
+  auto len2 = std::get<bencode::integer>(fL2);
+  auto pieceLen = std::get<bencode::integer>(filePieceLength); //bencoding the information
 
   try {
     boost::asio::io_context ioc;
